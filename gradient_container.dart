bba6e144619874689b53949e32be73ea53fa3de7
Reval Bernardtando <97012012+DonsCry@users.noreply.github.com>
2025-02-11 09:13:08 +0700

    Update gradient_container.dart

--- gradient_container.dart
+++ gradient_container.dart
@@ -1,41 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_teach/dice_roller.dart';
+import 'dart:math';
 
-// Konstanta untuk posisi awal dan akhir gradient
-const startAlignment = Alignment.topLeft; // Posisi awal gradient di pojok kiri atas
-const endAlignment = Alignment.bottomRight; // Posisi akhir gradient di pojok kanan bawah
+final randomizer = Random();
 
-// Kelas GradientContainer untuk membuat container dengan gradient
-class GradientContainer extends StatelessWidget {
-  // Konstruktor untuk membuat objek GradientContainer
-  const GradientContainer(this.color1, this.color2, {super.key});
+void main() {
+  runApp(const MyApp());
+}
 
-  // Variabel untuk warna pertama dan kedua gradient
-  final Color color1; // Warna pertama gradient
-  final Color color2; // Warna kedua gradient
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  // Fungsi untuk membuat widget
-  Widget build(context) {
-    // Membuat container dengan gradient
-    return Container(
-      // Dekorasi untuk container 
-      decoration: BoxDecoration(
-        // Membuat gradient
-        gradient: LinearGradient(
-          // Daftar warna untuk gradient
-          colors: [color1, color2],
-          // Posisi awal gradient
-          begin: startAlignment,
-          // Posisi akhir gradient
-          end: endAlignment,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice Roller',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DiceRoller(),
+    );
+  }
+}
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  final List<int> diceValues = [2, 2, 2];
+
+  void rollDice(int index) {
+    setState(() {
+      if (index == 0) {
+        diceValues[0] = randomizer.nextInt(6) + 1;
+      } else if (index == 1) {
+        diceValues[0] = randomizer.nextInt(6) + 1;
+        diceValues[1] = randomizer.nextInt(6) + 1;
+      } else {
+        diceValues[0] = randomizer.nextInt(6) + 1;
+        diceValues[1] = randomizer.nextInt(6) + 1;
+        diceValues[2] = randomizer.nextInt(6) + 1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dice Roller'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.filter_1)), // Tab 1 dadu
+              Tab(icon: Icon(Icons.filter_2)), // Tab 2 dadu
+              Tab(icon: Icon(Icons.filter_3)), // Tab 3 dadu
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: List.generate(3, (index) => DiceTab(
+                diceValues: diceValues.sublist(0, index + 1),
+                onRoll: () => rollDice(index),
+              )),
         ),
       ),
-      // Anak widget dari container
-      child: Center(
-        // Membuat widget DiceRoller di tengah-tengah container
-        child: DiceRoller(),
-      ),
+    );
+  }
+}
+
+class DiceTab extends StatelessWidget {
+  final List<int> diceValues;
+  final VoidCallback onRoll;
+
+  const DiceTab({super.key, required this.diceValues, required this.onRoll});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: diceValues
+              .map((value) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/images/dice-$value.png',
+                      width: 100,
+                    ),
+                  ))
+              .toList(),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: onRoll,
+          child: const Text('Roll Dice'),
+        ),
+      ],
     );
   }
 }
